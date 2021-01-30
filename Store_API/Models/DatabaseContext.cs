b954875bd9d28522f6d6800f4a80562d83b9bb58
parsalotfy <parsalotfy@outlook.com>
2021-01-31 00:25:44 +0330

    Add database context for accessing to tables using ef core

---
+++ Store_API/Models/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedProject;
+
+namespace Store_API.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+            //this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./StaticFiles/test.db");
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}

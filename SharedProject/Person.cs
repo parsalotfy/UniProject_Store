0ba8b6b0b89199d34b39d2b1d6d3fc743d99ac4b
parsalotfy <parsalotfy@outlook.com>
2021-02-04 21:53:28 +0330

    Add data anotation nuget package to support Key attribute

--- SharedProject/Person.cs
+++ SharedProject/Person.cs
@@ -1,9 +1,16 @@
-﻿namespace SharedProject
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedProject
 {
     public class Person
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public int Age { get; set; }
     }
 }

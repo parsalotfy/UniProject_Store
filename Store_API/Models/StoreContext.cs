3cc27c2efa205039c0d041691ed346eed2fb379e
parsalotfy <parsalotfy@outlook.com>
2021-06-02 14:57:04 +0430

    Rename the context file and remove sqlite configuration, because it is going to be done using dependency injection in Startup class

---
+++ Store_API/Models/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedProject;
+
+namespace Store_API.Models
+{
+    public class StoreContext : DbContext
+    {
+        
+         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+         {
+
+         }
+        
+        //public DbSet<Person> People { get; set; }
+        public DbSet<Person> People { get; set; }
+
+    }
+}

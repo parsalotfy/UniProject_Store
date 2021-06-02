f6ecf0c52bed64ae11b78f27cb53ff6914b325f1
parsalotfy <parsalotfy@outlook.com>
2021-06-02 14:58:18 +0430

    Add Sqlserver connection string and add StoreContext to the service container for dependency injection.

--- Store_API/Startup.cs
+++ Store_API/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Store_API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Store_API
 {
@@ -18,6 +20,21 @@ namespace Store_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            #region ForEFSqlServer
+            string server = Configuration["DBServer"] ?? "localhost";
+            string port = Configuration["DBPort"] ?? "8083";
+            string database = Configuration["Database"] ?? "Store";
+            string user = Configuration["DBUser"] ?? "sa";
+            string password = Configuration["DBPassword"] ?? "myPassword5";
+
+            string connectionString = $"Server={server},{port}; Initial Catalog={database}; User ID={user}; Password={password}";
+
+            services.AddDbContext<StoreContext>(options => 
+                options.UseSqlServer(connectionString));
+
+            #endregion ForEFSqlServer
+
             // https://stackoverflow.com/questions/64858434/net5-0-blazor-wasm-cors-client-exception
             // https://dzone.com/articles/cors-in-net-core-net-core-security-part-vi#:~:text=CORS%20stands%20for%20Cross%2DOrigin,origin%20requests%20while%20rejecting%20others.%22
             services.AddCors(options =>
